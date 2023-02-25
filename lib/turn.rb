18da4b1a17b3962bb2c60d38ed981e0a9fa455e7
Anna Marie Sterling <sterling.am@gmail.com>
2023-02-24 17:44:17 -0700

    Turn types

---
+++ lib/turn.rb
@@ -0,0 +1,62 @@
+require 'pry'
+
+class Turn
+  attr_reader :player1, :player2, :spoils_of_war
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def type
+    if (player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)) && (player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2))
+      :mutually_assured_destruction
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      :war
+    else player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      :basic
+    end
+  end
+
+  def winner
+    case type
+    when :basic
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        player1
+      else player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+        player2
+      end
+    else :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        player1
+      else player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+        player2
+      end
+    end
+  end
+
+  def pile_cards
+    case type
+    when :mutually_assured_destruction
+      2.times do
+        player1.deck.remove_card
+        player2.deck.remove_card
+      end
+    when :war
+      3.times do
+        spoils_of_war << player1.deck.remove_card
+        spoils_of_war << player2.deck.remove_card
+      end
+    else :basic
+      spoils_of_war << player1.deck.remove_card
+      spoils_of_war << player2.deck.remove_card
+    end
+  end
+
+  def award_spoils(winner)
+    winner.deck.cards << spoils_of_war
+    spoils_of_war = []
+    winner.deck.cards.flatten!
+  end
+end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,122 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require 'pry'
+
+RSpec.describe Turn do
+  let(:card1) { Card.new(:heart, 'Jack', 11) }
+  let(:card2) { Card.new(:heart, '10', 10) }
+  let(:card3) { Card.new(:heart, '9', 9) }
+  let(:card4) { Card.new(:diamond, 'Jack', 11) }
+  let(:card5) { Card.new(:heart, '8', 8) }
+  let(:card6) { Card.new(:diamond, 'Queen', 12) }
+  let(:card7) { Card.new(:heart, '3', 3) }
+  let(:card8) { Card.new(:diamond, '2', 2) }
+  let(:player1) { Player.new("Megan", deck1) }
+  let(:player2) { Player.new("Aurora", deck2) }
+  let(:turn) { Turn.new(player1, player2) }
+
+  context 'basic' do
+    let(:deck1) { Deck.new([card1, card2, card5, card8]) }
+    let(:deck2) { Deck.new([card3, card4, card6, card7]) }
+
+    it 'exists' do
+
+      expect(deck1).to be_an_instance_of(Deck)
+      expect(deck1.cards).to be_all(Card)
+      expect(deck2).to be_an_instance_of(Deck)
+      expect(deck2.cards).to be_all(Card)
+      expect(player1).to be_an_instance_of(Player)
+      expect(player2).to be_an_instance_of(Player)
+      expect(turn).to be_an_instance_of(Turn)
+    end
+
+    it 'has readable attributes' do
+
+      expect(turn.player1).to eq(player1)
+      expect(turn.player2).to eq(player2)
+      expect(turn.spoils_of_war).to eq([])
+    end
+
+    it 'is a basic type of turn' do
+
+      expect(turn.type).to eq(:basic)
+    end
+
+    it 'has a winner' do
+
+      expect(turn.winner).to eq(player1)
+    end
+
+    it 'has a pile of cards' do
+      turn.pile_cards
+
+      expect(turn.spoils_of_war).to eq([card1, card3])
+    end
+
+    it 'awards the pile cards to the winner' do
+
+      winner = turn.winner
+
+      turn.pile_cards
+
+      expect(turn.spoils_of_war).to eq([card1, card3])
+
+      turn.award_spoils(winner)
+
+      expect(player1.deck.cards).to eq([card2, card5, card8, card1, card3])
+      expect(player2.deck.cards).to eq([card4, card6, card7])
+    end
+  end
+
+  context 'war' do
+    let(:deck1) { Deck.new([card1, card2, card5, card8]) }
+    let(:deck2) { Deck.new([card4, card3, card6, card7]) }
+
+    it 'is a war type of turn' do
+
+      expect(turn.type).to eq(:war)
+    end
+
+    it 'awards the pile cards to the winner' do
+
+      winner = turn.winner
+
+      turn.pile_cards
+
+      expect(turn.spoils_of_war).to eq([card1, card4, card2, card3, card5, card6])
+
+      turn.award_spoils(winner)
+
+      expect(player1.deck.cards).to eq([card8])
+      expect(player2.deck.cards).to eq([card7, card1, card4, card2, card3, card5, card6])
+    end
+  end
+
+  context 'mutually assured destruction' do
+    let(:card6) { Card.new(:diamond, '8', 8) }
+    let(:deck1) { Deck.new([card1, card2, card5, card8]) }
+    let(:deck2) { Deck.new([card4, card3, card6, card7]) }
+
+    it 'is a war type of turn' do
+
+      expect(turn.type).to eq(:mutually_assured_destruction)
+    end
+
+    it 'awards the pile cards to the winner' do
+
+      winner = turn.winner
+
+      turn.pile_cards
+
+      expect(turn.spoils_of_war).to eq([])
+
+      turn.award_spoils(winner)
+
+      expect(player1.deck.cards).to eq([card5, card8])
+      expect(player2.deck.cards).to eq([card6, card7])
+    end
+  end
+end
